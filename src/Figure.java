0ff2cbd26a45961c2e0eea8df8f377327846c58c
Ivan <ikuptsovv@gmail.com>
2016-01-21 00:23:12 +0400

    Добавил базовый класс фигур

---
+++ src/Figure.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Ivan on 21.01.2016.
+ */
+public class Figure {
+    String name;//Каждая фигура имеет имя
+    int health,speed;// здоровье и скорость передвижения
+    //нужно прописать базовые методы
+}
