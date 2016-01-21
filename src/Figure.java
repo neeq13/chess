31d9081fe4eb11df14569c2e2346e1a49f810c45
IKuptsov <ikuptsovv@gmail.com>
2016-01-21 07:51:35 +0400

    добавил переменные

--- src/Figure.java
+++ src/Figure.java
@@ -2,7 +2,14 @@
  * Created by Ivan on 21.01.2016.
  */
 public class Figure {
-    String name;//Каждая фигура имеет имя
-    int health,speed;// здоровье и скорость передвижения
+    private String name;//Каждая фигура имеет имя
+    private int health,speed;// здоровье и скорость передвижения
+    private float exp;//переменная опыт фигуры
     //нужно прописать базовые методы
+    Figure(){//конструктор класса
+
+    }
+    public void move(){
+
+    }
 }
