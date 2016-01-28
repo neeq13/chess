4e24cc2ba854221421d0eaf7229feba1c4e223cd
Алексей Колюхов <trojanec_@mail.ru>
2016-01-28 15:56:46 +0200

    Прописан класс Pawn

--- ChessBand/core/src/com/mygdx/game/Figurs/Pawn.java
+++ ChessBand/core/src/com/mygdx/game/Figurs/Pawn.java
@@ -1,7 +1,20 @@
 package com.mygdx.game.Figurs;
 
-/**
- * Created by Алексей on 24.01.2016.
- */
-public class Pawn {
+
+public class Pawn extends Figure {
+
+    public Pawn(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public boolean setPosition(int x, int y) {
+        /*if (Math.abs(y-this.y) <=8&& x - this.x == 0|| y-this.y == 0&& Math.abs(x - this.x) <=8 ||y - this.y == x - this.x || -(y - this.y) == (x - this.x) ){
+            this.y = y;
+            this.x = x;
+            return true;
+        }*/
+        return false;
+    }
+
 }
