c51fb1afa1665203a16426fae3f9fd83856a80e1
RinxSR <rinxsr@gmail.com>
2016-02-01 18:15:52 +0400

    Подключил мышку

--- ChessBand/core/src/com/mygdx/game/GameClass.java
+++ ChessBand/core/src/com/mygdx/game/GameClass.java
@@ -10,6 +10,9 @@ public class GameClass extends ApplicationAdapter {
     SpriteBatch batch;
     Texture field;
     GameInit newGame;
+    int mouseX, mouseY;
+
+    Texture allocation;
 
     @Override
     public void create () {
@@ -17,10 +20,14 @@ public class GameClass extends ApplicationAdapter {
         field = new Texture("field.png");
         newGame = new GameInit();
 
+        allocation = new Texture("allocation.png");
+
     }
 
     @Override
     public void render () {
+        update();
+
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
@@ -31,6 +38,14 @@ public class GameClass extends ApplicationAdapter {
             batch.draw(newGame.figures.get(i).getTexture(), newGame.figures.get(i).getX() * 60, newGame.figures.get(i).getY() * 60);
         }
 
+        batch.draw(allocation, mouseX * 60, mouseY * 60);
+
         batch.end();
+    }
+
+    public void update() {
+        mouseX = Gdx.input.getX() / 60;
+        mouseY = (480 - Gdx.input.getY()) / 60;
+
     }
 }
