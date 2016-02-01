87b38f6373e079c76797cb502a16e17b1c062832
RinxSR <rinxsr@gmail.com>
2016-02-01 16:12:15 +0400

    Поправил видимость переменных + геттеры/сеттеры

--- ChessBand/core/src/com/mygdx/game/Figure.java
+++ ChessBand/core/src/com/mygdx/game/Figure.java
@@ -1,9 +1,44 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Texture;
+
 public abstract class Figure {
-    private int x;
-    private int y;
-    private char color;
+    protected int x;
+    protected int y;
+    protected char color;
+    protected Texture texture;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public char getColor() {
+        return color;
+    }
+
+    public void setColor(char color) {
+        this.color = color;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public void setTexture(Texture texture) {
+        this.texture = texture;
+    }
 
     public abstract void moveTo(int x, int y);
 
